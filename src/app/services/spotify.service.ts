52e500fb29903260ec2966e315b586fb8e851304
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-17 13:12:51 -0600

    Añadidos pipes a metodos de busqueda a spotify

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,13 +14,19 @@ export class SpotifyService {
      const HEADERS = new HttpHeaders({
        Authorization: 'Bearer BQCkCWwC_zUYfIQSJ0cenUPkyRKMlrSGNqLb9MMlv4iB0-BQd-8Sm-G5VfD-xxcH5DZJc7CQ7cIJ40ClHkM'
      });
-     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {  headers: HEADERS });
+     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {  headers: HEADERS })
+               .pipe( map( (data: any) => {
+                return data.albums.items;
+               }));
    }
 
    getArtista(termino: string) {
     const HEADERS = new HttpHeaders({
       Authorization: 'Bearer BQCkCWwC_zUYfIQSJ0cenUPkyRKMlrSGNqLb9MMlv4iB0-BQd-8Sm-G5VfD-xxcH5DZJc7CQ7cIJ40ClHkM'
     });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {  headers: HEADERS });
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {  headers: HEADERS })
+              .pipe( map( (data: any) =>{
+                return data.artists.items;
+              }));
    }
 }
