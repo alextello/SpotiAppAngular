e891193bc0b556df01eeef06c9314320b161af93
Edwin Alejandro Tello Santizo <alextello1@hotmail.com>
2020-03-17 13:19:53 -0600

    Optimización de código

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -10,22 +10,25 @@ export class SpotifyService {
   constructor(private http: HttpClient) {
    }
 
-   getNewReleases(){
-     const HEADERS = new HttpHeaders({
-       Authorization: 'Bearer BQCkCWwC_zUYfIQSJ0cenUPkyRKMlrSGNqLb9MMlv4iB0-BQd-8Sm-G5VfD-xxcH5DZJc7CQ7cIJ40ClHkM'
-     });
-     return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {  headers: HEADERS })
-               .pipe( map( (data: any) => {
-                return data.albums.items;
-               }));
+   getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${ query }`;
+    const headers = new HttpHeaders({
+      Authorization: 'Bearer BQCkCWwC_zUYfIQSJ0cenUPkyRKMlrSGNqLb9MMlv4iB0-BQd-8Sm-G5VfD-xxcH5DZJc7CQ7cIJ40ClHkM'
+    });
+    return this.http.get(url, {headers});
+   }
+
+   getNewReleases() {
+
+     return this.getQuery('browse/new-releases?limit=20')
+                  .pipe( map( (data: any) => {
+                  return data.albums.items;
+                 }));
    }
 
    getArtista(termino: string) {
-    const HEADERS = new HttpHeaders({
-      Authorization: 'Bearer BQCkCWwC_zUYfIQSJ0cenUPkyRKMlrSGNqLb9MMlv4iB0-BQd-8Sm-G5VfD-xxcH5DZJc7CQ7cIJ40ClHkM'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, {  headers: HEADERS })
-              .pipe( map( (data: any) =>{
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+              .pipe( map( (data: any) => {
                 return data.artists.items;
               }));
    }
